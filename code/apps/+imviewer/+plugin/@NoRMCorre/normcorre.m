3576de5f21a55e84c83d953a537ae27bc4a16330
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-03-17 10:59:15 +0100

    temp rename file to fix capitalization

--- code/apps/+imviewer/+plugin/@NoRMCorre/normcorre.m
+++
@@ -1,7 +0,0 @@
-classdef NoRMCorre < nansen.plugin.imviewer.NoRMCorre
-    methods
-        function obj = NoRMCorre(varargin)
-            obj@nansen.plugin.imviewer.NoRMCorre(varargin{:})
-        end
-    end
-end
